a0dc6f78ea302b7c00591ec781db80a4724918b1
PiratheeshG <piratheesh.gunaseelan@ontariotechu.net>
2024-12-03 19:02:41 -0500

    Fixed small error

--- beatmiles-backend/routes/workouts.js
+++ beatmiles-backend/routes/workouts.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Middleware to verify JWT
 const authenticate = async (req, res, next) => {
